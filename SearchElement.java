9a06268ffd294f30a34531d7e33ef139c5f5e7ee
Kavyanadesan <144669130+Kavyanadesan@users.noreply.github.com>
2024-05-01 13:12:05 +0530

    Add files via upload

---
+++ SearchElement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Search{
+    public static void main(String arg[]){
+		System.out.println("Name : KAVYA V N\nRoll.no : 38\nTitle : Search element in array\nDate : 13/02/2024\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements : ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Array is : " + Arrays.toString(arr));
+        System.out.print("Enter the value to search : ");
+        int value = scanner.nextInt();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            if(value == arr[i]){
+                flag = 1;
+                break;
+            }
+        }
+        System.out.println("Element " + value + (flag == 0 ? " not found in the array" : " found in the array"));
+
+        scanner.close();
+    }
+}
